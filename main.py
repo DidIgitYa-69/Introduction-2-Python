13ea663ca429c19e4bc2bc440aebb6c67cdae1cf
DidIgitYa-69 <nazeer.faizan@gmail.com>
2026-02-05 07:07:13 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+#printing my epikk name
+print("Mah name is Ibraheem Faizan")
+
+#printing some awesome facts about mahself
+print("i like footy and art")
+
+#printing a simple equation
+print("67 + 67 - 67 x 67 / 67 = ", 67 + 67 - 67 * 67 / 67)
